14afaa394f4d2a2ebbbd7c8ecc6a6ba9ef03c617
Sergey Morenets <sergey.morenets@gmail.com>
2018-07-21 11:26:47 +0300

    Task #1

--- spring-task1/src/main/java/it/discovery/loader/BootstrapLoader.java
+++ spring-task1/src/main/java/it/discovery/loader/BootstrapLoader.java
@@ -3,12 +3,18 @@ package it.discovery.loader;
 import java.util.List;
 
 import it.discovery.model.Book;
-import it.discovery.service.MainBookService;
+import it.discovery.repository.BookRepository;
+import it.discovery.repository.DBBookRepository;
+import it.discovery.service.BookService;
+import it.discovery.service.BookServiceImpl;
 
 public class BootstrapLoader {
 
 	public static void main(String[] args) {
-		MainBookService service = new MainBookService();
+		DBBookRepository bookRepository = new DBBookRepository();
+		bookRepository.setDb("library");
+		bookRepository.setServer("localhost");
+		BookService service = new BookServiceImpl(bookRepository);
 		Book book = new Book();
 		book.setName("Introduction into Spring");
 		book.setPages(100);
---
+++ spring-task1/src/main/java/it/discovery/service/BookServiceImpl.java
@@ -0,0 +1,31 @@
+package it.discovery.service;
+
+import java.util.List;
+
+import it.discovery.model.Book;
+import it.discovery.repository.BookRepository;
+import it.discovery.repository.DBBookRepository;
+
+public class BookServiceImpl implements BookService {
+    private final BookRepository repository;
+
+    public BookServiceImpl(BookRepository repository) {
+        this.repository = repository;
+        System.out.println("Using db repository");
+    }
+
+    @Override
+    public void saveBook(Book book) {
+        repository.saveBook(book);
+    }
+
+    @Override
+    public Book findBookById(int id) {
+        return repository.findBookById(id);
+    }
+
+    @Override
+    public List<Book> findBooks() {
+        return repository.findBooks();
+    }
+}
