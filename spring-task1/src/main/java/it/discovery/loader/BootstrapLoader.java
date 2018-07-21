7e107afb3d39233699ad119ff62095e9b4a50e73
Sergey Morenets <sergey.morenets@gmail.com>
2018-07-21 12:00:07 +0300

    Task #2

--- spring-task1/src/main/java/it/discovery/loader/BootstrapLoader.java
+++ spring-task1/src/main/java/it/discovery/loader/BootstrapLoader.java
@@ -1,5 +1,6 @@
 package it.discovery.loader;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
 import it.discovery.model.Book;
@@ -10,19 +11,23 @@ import it.discovery.service.BookServiceImpl;
 
 public class BootstrapLoader {
 
-	public static void main(String[] args) {
-		DBBookRepository bookRepository = new DBBookRepository();
-		bookRepository.setDb("library");
-		bookRepository.setServer("localhost");
-		BookService service = new BookServiceImpl(bookRepository);
-		Book book = new Book();
-		book.setName("Introduction into Spring");
-		book.setPages(100);
-		book.setYear(2016);
-		service.saveBook(book);
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        DBBookRepository bookRepository = new DBBookRepository();
+        bookRepository.setDb("library");
+        bookRepository.setServer("localhost");
+        BookService service = new BookServiceImpl(bookRepository);
+        Book book = new Book();
+        book.setName("Introduction into Spring");
+        book.setPages(100);
+        book.setYear(2016);
+        service.saveBook(book);
 
-		List<Book> books = service.findBooks();
-		System.out.println(books);
-	}
+        List<Book> books = service.findBooks();
+        System.out.println(books);
+
+//        Class<?> clz = Class.forName("it.discovery.model.Book");
+//        Object obj = clz.getDeclaredConstructors()[0].newInstance();
+//        System.out.println(obj.getClass());
+    }
 
 }
